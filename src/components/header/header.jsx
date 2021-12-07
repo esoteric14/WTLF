6d5e67dcb79b9c3ab9b4d2f606fcebd5a0590e72
Ayesha Ayub <ayubayesha07@gmail.com>
2021-12-08 05:25:06 +0530

    home page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,9 +9,9 @@ const header = () => {
             <nav>
                 <h3>WTLF.club</h3>
                 <ul>
-                    <Link to="/"> <li>WhatTLF?</li></Link>
-                    <Link to="/WhoTLF"> <li>WhoTLF?</li></Link>
-                    <Link to="/WhichTLF"> <li>WhichTLF?</li></Link>
+                    <Link className="link-menu" to="/"> <li>WhatTLF?</li></Link>
+                    <Link className="link-menu" to="/WhoTLF"> <li>WhoTLF?</li></Link>
+                    <Link className="link-menu" to="/WhichTLF"> <li>WhichTLF?</li></Link>
                 </ul>
             </nav>
         </div>
