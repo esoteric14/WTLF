a731641c313eb31ff0637d955a01d106d010fc80
yaswanth-git <yaswanth.offl@gmail.com>
2021-12-12 19:40:23 +0530

    mobile desing for who

--- src/components/who/whotlf.jsx
+++ src/components/who/whotlf.jsx
@@ -4,59 +4,69 @@ import chin from "../../assets/chin.png";
 import painting from "../../assets/painting.png";
 import "./whotlf.css";
 import Fade from "react-reveal/Fade";
-import { Div } from "./whotlf.styled";
+
+const ShortText = (props) => {
+  return (
+    <div className={`text-container ${props.superClass}`}>
+      <p>"Why </p>
+      <p>the long</p>
+      <p>face"</p>
+      {props.children}
+    </div>
+  );
+};
 
 const Whotlf = () => {
   const [reveal, setReveal] = useState(false);
-  const [scrollPos, setScrollPos] = useState(0);
-  const handleScroll = () => {
-    let scrollTop = window.scrollY;
-    let docHeight = document.body.offsetHeight;
-    let winHeight = window.innerHeight;
-    let scrollPercent = scrollTop / (docHeight - winHeight);
-    scrollPercent = Math.round(scrollPercent * 100);
-    console.log(scrollPercent);
-    setScrollPos(scrollPercent);
-  };
+  const [expand, setExpand] = useState(false);
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    const timer = setTimeout(() => setExpand(true), 1000);
+    return () => clearTimeout(timer);
   }, []);
-  useEffect(() => {
-    if (scrollPos > 50) {
-      setReveal(true);
-    } else {
-      setReveal(false);
-    }
-  }, [scrollPos]);
   return (
-    <div className="who-tlf" onScroll={() => handleScroll()}>
-      <div className="text-and-image-wrapper">
-        <div className="text-container">
-          <p>"Why </p>
-          <p>the long</p>
-          <p>face"</p>
+    <div className="who-tlf">
+      <ShortText superClass="desktop">
+        <Fade right when={reveal}>
+          <img src={painting} alt="" className="fade-image" />
+        </Fade>
+      </ShortText>
+      <div className="image-container">
+        <img src={forehead} alt="missing" className="fore-head" />
+        <ShortText superClass={`mobile ${expand ? "shrinked" : "expanded"}`} />
+        <div
+          className={`text-between-images ${expand ? "expanded" : "shrinked"}`}
+        >
+          <p>
+            When
+            <span className="logicbomb" onClick={() => setReveal(true)}>
+              {" "}
+              //logikbomb
+            </span>{" "}
+            asked
+          </p>
+          <p>
+            <span className="partmeister" onClick={() => setReveal(true)}>
+              //partmeister
+            </span>{" "}
+            this rather innocuous question, back
+          </p>
+          <p> in 2011, the answer was a set of graphic</p>
+          <p> illustrations that proved that ‘a long face’ can be</p>
+          <p> more than just one emotion. They could be cool, or</p>
+          <p>cruel. Mischievous or morbid. Funny or furious.</p>
+          <p> hundreds of design re-re-re-tweaks later,</p>{" "}
+          <p>the Rogues Collective proudly released</p>
+          <p>the WTLF project</p>
           <Fade right when={reveal}>
-            <img src={painting} alt="" className="fade-image" />
+            <div className="mobile-img-wrapper">
+              <div className="flex-container">
+                <img src={painting} alt="" className="fade-image-mobile" />
+                <div className="black-area-close"></div>
+              </div>
+            </div>
           </Fade>
         </div>
-        <div className="image-container">
-          <img src={forehead} alt="missing" className="fore-head" />
-          <Div height={scrollPos * 3.08 * 2}>
-            <p>When //logikbomb asked</p>
-            <p> //partmeister this rather innocuous question, back</p>
-            <p> in 2011, the answer was a set of graphic</p>
-            <p> illustrations that proved that ‘a long face’ can be</p>
-            <p> more than just one emotion. They could be cool, or</p>
-            <p>cruel. Mischievous or morbid. Funny or furious.</p>
-            <p> hundreds of design re-re-re-tweaks later,</p>{" "}
-            <p>the Rogues Collective proudly released</p>
-            <p>the WTLF project</p>
-          </Div>
-          <img src={chin} alt="missing" className="chin" />
-        </div>
+        <img src={chin} alt="missing" className="chin" />
       </div>
     </div>
   );
