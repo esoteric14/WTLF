fa34ca2d7d4eaf721d8ab9ec78135659d1c6f3bc
Tipu <tipualikhan@gmail.com>
2022-01-20 18:36:47 +0530

    Added FB link

--- src/components/where/wheretlf.jsx
+++ src/components/where/wheretlf.jsx
@@ -6,7 +6,7 @@ const Whotlf = () => {
         <section className='social'>
             <ul className='social-container'>
                 <li className='fb'>
-                    <a title='Facebook'>
+                    <a href="https://www.facebook.com/Rogues-Collective-100891032500527/"  rel="noopener noreferrer" title='Facebook'>
                         <img src="/social/fb.png" alt="fb" />
                         <span>rogues collective<br /><span style={{fontSize: '20px', fontWeight: '100'}}>(Pages)</span></span>
                     </a>
